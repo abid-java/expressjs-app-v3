f84e9ab96ed67d8e965de994b98309a1b858d346
abmohamm <mohd.92java@gmail.com>
2021-09-30 13:00:02 -0400

    Added few more routes

--- routes/dist/index.dev.js
+++ routes/dist/index.dev.js
@@ -1,26 +1,29 @@
-var express = require('express');
-var router = express.Router();
+"use strict";
 
+var express = require('express');
+
+var router = express.Router();
 /* GET home page. */
 //http://localhost:3000/
+
 router.get('/', function (request, response, next) {
   response.render('index', {
     title: 'Express',
     subtitle: 'Welcome to best Youtube Audience'
   });
 });
-
 /* GET home page. */
 //http://localhost:3000/angular
+
 router.get('/angular', function (request, response, next) {
   response.render('index', {
     title: 'Express - Angular',
     subtitle: 'Welcome to best Angular Audience'
   });
 });
-
 /* GET home page. */
 //http://localhost:3000/tutorials?course=angular
+
 router.get('/tutorials', function (request, response, next) {
   console.log("<===== Request Query Parameters =====>");
   console.log(request.query);
@@ -30,7 +33,5 @@ router.get('/tutorials', function (request, response, next) {
     subtitle: 'Welcome to best Angular Audience'
   });
 });
-
-
-
 module.exports = router;
+//# sourceMappingURL=index.dev.js.map
