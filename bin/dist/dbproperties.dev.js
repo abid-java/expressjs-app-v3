bce72bbbcad78a97e66a862786645b38e47cc7a4
abmohamm <mohd.92java@gmail.com>
2021-09-30 17:33:14 -0400

    database connection changes

---
+++ bin/dist/dbproperties.dev.js
@@ -0,0 +1,6 @@
+"use strict";
+
+module.exports = {
+  DB_URL: "mongodb://localhost:27017/local"
+};
+//# sourceMappingURL=dbproperties.dev.js.map
---
+++ routes/testroutes.js
@@ -0,0 +1,33 @@
+/*jshint esversion: 6 */
+var express = require("express");
+var router = express.Router();
+
+/* GET home page. */
+//http://localhost:3000/
+router.get('/', function (request, response, next) {
+    response.render('index', {
+      title: 'Express',
+      subtitle: 'Welcome to best Youtube Audience'
+    });
+  });
+  
+  /* GET home page. */
+  //http://localhost:3000/angular
+  router.get('/angular', function (request, response, next) {
+    response.render('index', {
+      title: 'Express - Angular',
+      subtitle: 'Welcome to best Angular Audience'
+    });
+  });
+  
+  /* GET home page. */
+  //http://localhost:3000/tutorials?course=angular
+  router.get('/tutorials', function (request, response, next) {
+    console.log("<===== Request Query Parameters =====>");
+    console.log(request.query);
+    var courseName = request.query.course;
+    response.render('index', {
+      title: 'Express - ' + courseName,
+      subtitle: 'Welcome to best Angular Audience'
+    });
+  });
