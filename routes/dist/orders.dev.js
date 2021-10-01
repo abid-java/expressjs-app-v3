6358e8972bb73a658102be25e7140942a145b2e4
abmohamm <mohd.92java@gmail.com>
2021-09-30 21:45:43 -0400

    created skeleton application structure

---
+++ routes/dist/orders.dev.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var express = require('express');
+
+var router = express.Router();
+/* GET orders listing. */
+
+router.get('/', function (request, response, next) {
+  response.send('respond with a resource');
+});
+module.exports = router;
+//# sourceMappingURL=orders.dev.js.map
